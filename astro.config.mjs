e9c868594dff1a4f6de2e2159abd80db3538940e
Matt Riley <matt@mattriley.me>
2022-02-19 14:11:32 +0000

    feat: uses react and hydrates on client

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ export default {
     site: "https://upbeat-meninsky-153a98.netlify.app/", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     sitemap: true, // Generate sitemap (set to "false" to disable)
   },
-  renderers: ["@astrojs/renderer-svelte"],
+  renderers: ["@astrojs/renderer-react"],
 };
